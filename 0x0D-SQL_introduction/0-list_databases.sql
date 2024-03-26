14b6980a2d8354df59dbe689d5a7ecc3780988e4
MakedaF <makedareda@gmail.com>
2024-03-26 14:51:45 -0700

    take07

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,2 @@
 -- List all databases of MySQL server
-SHOW DATABASES;
+SELECT name FROM sys. databases;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- deletes database hbtn_0c_0 from MYSQL server
-DROP IF EXISTS hbtn_0c_0;
+DROP EXISTS hbtn_0c_0;

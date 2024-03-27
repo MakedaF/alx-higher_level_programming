7297c518eb76a396653fd01bc1248b05e6d6cd17
MakedaF <makedareda@gmail.com>
2024-03-27 11:35:34 -0700

    03

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
---Creates hbtn_0d_2 datbase and user_0d_02
---Create database
+-- Creates hbtn_0d_2 datbase and user_0d_02
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
---Create user
-CREATE USER IF NOT EXISTS user_0d_2@hbtn_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
---Grant SELECT privilege
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@hbtn_0d_2;
+-- Create user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant SELECT privilege
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

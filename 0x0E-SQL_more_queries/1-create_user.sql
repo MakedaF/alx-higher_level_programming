0793f9efc8e1089b7c7952310a7651b78e35bc38
MakedaF <makedareda@gmail.com>
2024-03-27 10:53:27 -0700

    03

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
 --creates user "user 0d 1" with all privilages
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
---Grant privilege
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
-FLUSH PRIVILEGES;
-
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;

8ddc947fa5558e532d0ba3865c163c8cf4c38e39
MakedaF <makedareda@gmail.com>
2024-03-28 03:53:55 -0700

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
--- create table in the current database
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256)); 
+-- Create a table first_table in the current database
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256)); 

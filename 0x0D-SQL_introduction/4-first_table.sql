beb1e39b82977d27eed3d4e4432dfbc203d67bc3
MakedaF <makedareda@gmail.com>
2024-03-28 03:11:43 -0700

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,2 @@
 --create table in the current database
--- create database
-CREATE IF NOT EXISTS DATABASE hbtn_0c_0;
---set current database
-USE hbtn_0c_0;
---create table
-CREATE TABLE IF NOT EXISTS first_table (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id)); 
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256)); 

cde207da87d7b9d08ded9ae700c50f5963dd3e0a
MakedaF <makedareda@gmail.com>
2024-04-22 13:54:18 -0700

    concat01

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
-  console.log('undefined is' + process.argv[3]);
+  console.log('undefined is undefined');
 } else if (process.argv[3] === undefined) {
   console.log(process.argv[2] + 'is undefined');
 } else {

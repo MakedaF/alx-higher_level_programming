868237a8798625faef9dd70216b519c23375b50f
MakedaF <makedareda@gmail.com>
2024-04-22 13:22:14 -0700

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const NumOfArgs = process.argv.length - 2;
+if (NumOfArgs === 0) {
+  console.log('No argument');
+} else if (NumOfArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}

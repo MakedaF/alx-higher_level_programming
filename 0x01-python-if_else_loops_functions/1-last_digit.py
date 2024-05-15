b2e268709ca4bad114c29be1200f7b9bb7c0d83a
MakedaF <makedareda@gmail.com>
2024-05-15 05:16:26 -0700

    worked

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,11 @@ if (x % 10) == 0:
     a = f"Last digit of {number} is {x % 10}"
     a0 = " and is 0"
     print(a + a0)
-elif (x % 10) != 0 and (x % 10) < 6:
+elif number < 0:
+    a = f"Last digit of {number} is {-1 *(x % 10)}"
+    a1 = " and is less than 6 and not 0"
+    print(a + a1)
+elif number > 0 and (x % 10) != 0 and (x % 10) < 6:
     a = f"Last digit of {number} is {number % 10}"
     a1 = " and is less than 6 and not 0"
     print(a + a1)

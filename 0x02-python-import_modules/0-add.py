f50fb91155d54dbcfada24769ec06a73ca451ff1
MakedaF <makedareda@gmail.com>
2024-05-17 05:36:23 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,7 @@ Imports function add(a, b) from module add_0.py
 """
 
 if __name__ == "__main__":
-    import add_0 as sum01
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, sum01.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))

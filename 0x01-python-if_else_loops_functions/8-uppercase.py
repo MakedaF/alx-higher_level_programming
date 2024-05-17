cbb7738c8602d59a5a10425564d9c39095e4bb64
MakedaF <makedareda@gmail.com>
2024-05-17 02:18:36 -0700

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,10 +3,13 @@
 prints uppercase followed by a new line
 """
 
+
 def uppercase(str):
-    for x in range(len(str)):
-       # if ord(str[x]) in range(65, 91):
-           # print("{}".format(str[x]), end="")
-        if ord(str[x]) in range(97, 123):
-            print("{}".format(chr(ord(str[x]) - 32)), end="")
-    print()
+    result = ""
+    for x in str:
+        if ord(x) in range(97, 123):
+            upper_char = chr(ord(x) - 32)
+        else:
+            upper_char = x
+        result += upper_char
+    print("{}".format(result))
